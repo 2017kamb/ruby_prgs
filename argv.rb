a5aaef6dae1d74e4e823dbcc24b5a61937bbd347
Raj Kumar <2017kamb@gmail.com>
2022-05-11 04:18:18 +0530

    add two programs

---
+++ argv.rb
@@ -0,0 +1,9 @@
+puts ARGV.join('|||')
+puts ARGV[0]
+puts ARGV[5]
+puts ARGV.length
+# rk@rk-Vostro-3559:~/workspace/ruby_prgs$ ruby argv.rb My name is Raj Kumar Sharma.
+# My|||name|||is|||Raj|||Kumar|||Sharma.
+# My
+# Sharma.
+# 6
